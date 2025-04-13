b549c10ed185afe196cf904f29c9d015e0d317ca
Siddhant Mishra <151660891+heyysiddhant@users.noreply.github.com>
2025-04-14 00:56:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,87 @@
+var timer =60;
+var score =0;
+var hitrn =0;
+
+function increaseScore(){
+  score +=10;
+  document.querySelector("#scoreval").textContent = score;
+}
+
+function getNewHit(){
+  hitrn = Math.floor(Math.random () * 10);
+  document.querySelector("#hitval").textContent = hitrn;
+}
+
+function makeBubble() {
+
+  var clutter = "";
+  
+  for(var i=1; i<=119; i++){
+      
+   var rn = Math.floor(Math.random()*10)
+   clutter += `<div class="bubble">${rn}</div>` ;
+  
+  }
+  
+  document.querySelector("#pbtm").innerHTML = clutter;
+  
+  }
+  
+function runTimer() {
+  var timerint = setInterval(function(){
+    if(timer>0){
+      timer--;
+    document.querySelector("#timerval").textContent =timer;
+  }
+  else{
+    clearInterval(timerint);
+    document.querySelector("#pbtm").innerHTML =`<h2>Game Over</h2>`
+  }
+  },1000);
+
+}
+
+document.querySelector("#pbtm")
+.addEventListener("click", function(e){
+  var clickednum = Number(e.target.textContent);
+  if(clickednum === hitrn){
+    increaseScore();
+    makeBubble();
+    getNewHit();
+  }
+});
+
+getNewHit();
+runTimer();
+makeBubble();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*practice
+
+for(var i=0; i<=10; i++){
+
+  var v =Math.floor( Math.random()*10);
+    console.log(v) 
+
+     } */
